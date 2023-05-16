b7e2a519914f321971f615e1ec4145f98167c55f
최한울[유저플랫폼개발] <hanul.choi95@nts-corp.com>
2023-05-16 23:01:56 +0900

    contents fade out

--- index.js
+++ index.js
@@ -40,6 +40,54 @@ function scrollIntoView(selector) {
     scrollTo.scrollIntoView({ behavior: "smooth" });
 }
 
+function fadeOut() {
+    window.addEventListener('scroll', (event) => {
+        const homeElement = document.querySelector('.home__container')
+        const homeScrollHeight = homeElement.scrollHeight
+
+        if (window.scrollY > 0 && window.scrollY < homeScrollHeight) {
+            homeElement.style.opacity = 1-(window.scrollY/homeScrollHeight)
+        }
+
+        const aboutElement = document.querySelector('#about')
+        const aboutScrollHeight = aboutElement.scrollHeight;
+        let prevSum = homeScrollHeight;
+        let sum = homeScrollHeight + aboutScrollHeight;
+
+        if (window.scrollY > prevSum && window.scrollY < sum) {
+            aboutElement.style.opacity = 1-((window.scrollY - prevSum)/aboutScrollHeight)
+        }
+
+        const skillsElement = document.querySelector('#skills .section__container')
+        const skillsScrollHeight = skillsElement.scrollHeight;
+        prevSum = homeScrollHeight + aboutScrollHeight;
+        sum = homeScrollHeight + aboutScrollHeight + skillsScrollHeight;
+
+        if (window.scrollY > prevSum && window.scrollY < sum) {
+            skillsElement.style.opacity = 1-((window.scrollY - prevSum)/skillsScrollHeight)
+        }
+
+        const workElement = document.querySelector('#work .section__container')
+        const workScrollHeight = workElement.scrollHeight;
+        prevSum = homeScrollHeight + aboutScrollHeight + skillsScrollHeight;
+        sum = homeScrollHeight + aboutScrollHeight + skillsScrollHeight + workScrollHeight;
+
+        if (window.scrollY > prevSum && window.scrollY < sum) {
+            workElement.style.opacity = 1-((window.scrollY - prevSum)/workScrollHeight)
+        }
+
+        const testElement = document.querySelector('#testimonials .section__container')
+        const testScrollHeight = testElement.scrollHeight;
+        prevSum = homeScrollHeight + aboutScrollHeight + skillsScrollHeight + workScrollHeight;
+        sum = homeScrollHeight + aboutScrollHeight + skillsScrollHeight + workScrollHeight + testScrollHeight;
+
+        if (window.scrollY > prevSum && window.scrollY < sum) {
+            testElement.style.opacity = 1-((window.scrollY - prevSum)/testScrollHeight)
+        }
+    })
+}
+
 fixNavbar();
 handleClickNavMenu();
 handleClickContactMe();
+fadeOut();
