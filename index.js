e34f0f1d990abe7c67128a3a85e9cc46f7e9a25d
최한울[유저플랫폼개발] <hanul.choi95@nts-corp.com>
2023-05-16 22:03:50 +0900

    [ellie] scroll to section
    - li 태그를 감싸고 있는 ul태그에 addEventListener로 click 이벤트를 걸고
    event 인자로 현재 클릭된 요소의 값을 가져올 수 있다…!!
    - data 속성을 사용하면 추후 event.target.dataset 으로 값을 가져올 수 있다

--- index.js
+++ index.js
@@ -12,54 +12,19 @@ function fixNavbar() {
     })
 }
 
+// Handle scrolling when tapping on the navbar menu
 function handleClickNavMenu() {
-    const home = document.querySelector('#home')
-    const about = document.querySelector('#about')
-    const skills = document.querySelector('#skills')
-    const work = document.querySelector('#work')
-    const testimonials = document.querySelector('#testimonials')
-    const contact = document.querySelector('#contact')
-    const contactMe = document.querySelector('.home__contact');
+    const navbarMenu = document.querySelector('.navbar__menu');
+    navbarMenu.addEventListener('click', (event) => {
+        const target = event.target;
+        const link = target.dataset.link;
 
-    const navbarMenuItems = document.querySelectorAll('.navbar__menu__item');
+        if (!link) {
+            return;
+        }
 
-    navbarMenuItems.forEach(item => {
-        item.addEventListener('click', () => {  
-            navbarMenuItems.forEach(menuItem => {
-                menuItem.classList.remove('active');
-            })
-            
-            switch (item.textContent) {
-                case 'Home':
-                    item.classList.add('active');
-                    home.scrollIntoView({ behavior: "smooth" });
-                    break;
-                case 'About':
-                    item.classList.add('active');
-                    about.scrollIntoView({ behavior: "smooth" });
-                    break;
-                case 'Skills':
-                    item.classList.add('active');
-                    skills.scrollIntoView({ behavior: "smooth" });
-                    break;
-                case 'My work':
-                    item.classList.add('active');
-                    work.scrollIntoView({ behavior: "smooth" });
-                    break;
-                case 'Testimonials':
-                    item.classList.add('active');
-                    testimonials.scrollIntoView({ behavior: "smooth" });
-                    break;
-                case 'Contact':
-                    item.classList.add('active');
-                    contact.scrollIntoView({ behavior: "smooth" });
-                    break;
-            }
-        })
-    })
-
-    contactMe.addEventListener('click', () => {
-        contact.scrollIntoView({ behavior: "smooth" });
+        const scrollTo = document.querySelector(link);
+        scrollTo.scrollIntoView({ behavior: "smooth" });
     })
 }
 
