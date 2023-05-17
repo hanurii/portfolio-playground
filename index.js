edf1fe296f2e9c0f84a815d2eff64938c7bf2435
최한울[유저플랫폼개발] <hanul.choi95@nts-corp.com>
2023-05-17 17:16:50 +0900

    Arrow up button

--- index.js
+++ index.js
@@ -51,8 +51,37 @@ function fadeOut() {
     })
 }
 
+function goToTop() {
+    const goToTopBtn = document.querySelector('.btn__go__top');
+
+    document.addEventListener('scroll', () => {
+        if (window.scrollY < 300) {
+            goToTopBtn.style.visibility = 'hidden'
+        } else {
+            goToTopBtn.style.visibility = 'visible'
+            goToTopBtn.style.opacity = 0 + (window.scrollY / 500)
+        }
+    })
+
+    goToTopBtn.addEventListener('click', debounce(() => {
+        document.querySelector('#home').scrollIntoView({ behavior: 'smooth' });
+    }, 200))
+}
+
 fixNavbar();
 handleClickNavMenu();
 handleClickContactMe();
 fadeOut();
+goToTop();
 
+function debounce (handler, delay) {
+    let debouncer = null
+    return function () {
+      // 이벤트가 발생하면 타임아웃을 초기화
+        clearTimeout(debouncer)
+      // 이후 다시 타임아웃을 걸어준다
+        debouncer = setTimeout(() => {
+            handler.apply(this, arguments)
+        }, delay)
+    }
+}
